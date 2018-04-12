d438d48c3aeec02a73d58bd2fc2ed00670253cca
Lucas Polo <pradopolo@gmail.com>
2018-04-12 12:25:06 -0300

    Ajuste do mínimo de upvotes

--- crawlers/reddit_crawler.py
+++ crawlers/reddit_crawler.py
@@ -1,11 +1,17 @@
+import sys
 from requests_html import HTMLSession
 
-BASE_URL = "https://www.reddit.com/"
+BASE_URL = "https://www.reddit.com"
 BASE_SUBREDDIT = BASE_URL + "/r/{0}"
 
 session = HTMLSession()
 
-def get_subreddit(subreddit):
+
+def convert_upvotes_to_num(votes):
+    return int(votes.replace("k", "000"))
+
+
+def get_threads(subreddit):
 
     r = session.get(BASE_SUBREDDIT.format(subreddit))
 
@@ -18,9 +24,14 @@ def get_subreddit(subreddit):
 
         thread["subreddit"] = element.attrs["data-subreddit"]
         thread["titulo"] = element.find("a.title", first=True).text
-        thread["upvotes"] = element.attrs["data-score"]
+        thread["upvotes"] = convert_upvotes_to_num(element.attrs["data-score"])
         thread["comments"] = element.attrs["data-permalink"]
         thread["link"] = element.attrs["data-url"]
+
+        if element.attrs["data-domain"] == f"self.{subreddit}":
+            thread["external"] = False
+        else
+            thread["external"] = True
 
         threads.append(thread)
 
@@ -34,5 +45,12 @@ def print_subreddits(threads):
         print(f"\tLink: {thread['link']}")
         print(f"\tComments: {thread['comments']}\n")
 
+def filter_by_votes(threads, min_votes=5000):
+    return [thread for thread in threads if thread['upvotes'] > min_votes]
+
+def main(subreddits):
+    for subreddit in subreddits:
+        print_subreddits(filter_by_votes(get_threads(subreddit)))
+
 if __name__ == '__main__':
-    print_subreddits(get_subreddit("Python"))
+    main(sys.argv[1].split(";"))
