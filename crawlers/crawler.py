748c5a92204d7f8c666052cbcc6fa0f0e2467fc9
Lucas Polo <pradopolo@gmail.com>
2018-04-11 22:44:29 -0300

    Create function get_subreddit and print_subreddits

--- crawlers/crawler.py
+++ crawlers/crawler.py
@@ -1,23 +1,38 @@
 from requests_html import HTMLSession
 
-BASE_URL = "https://www.reddit.com/r/{0}/"
+BASE_URL = "https://www.reddit.com/"
+BASE_SUBREDDIT = BASE_URL + "/r/{0}"
 
 session = HTMLSession()
 
-r = session.get(BASE_URL.format("python"))
+def get_subreddit(subreddit):
 
-elements_threads = r.html.find(".thing")
+    r = session.get(BASE_SUBREDDIT.format(subreddit))
 
-threads = []
+    elements_threads = r.html.find(".thing")
 
-for element in elements_threads:
-    thread = {}
+    threads = []
 
-    thread["subreddit"] = element.attrs["data-subreddit"]
-    thread["titulo"] = element.find("a.title", first=True).text
-    thread["upvotes"] = element.attrs["data-score"]
-    thread["comments"] = element.attrs["data-permalink"]
+    for element in elements_threads:
+        thread = {}
 
-    print(thread)
+        thread["subreddit"] = element.attrs["data-subreddit"]
+        thread["titulo"] = element.find("a.title", first=True).text
+        thread["upvotes"] = element.attrs["data-score"]
+        thread["comments"] = element.attrs["data-permalink"]
+        thread["link"] = element.attrs["data-url"]
 
-    threads.append(thread)
+        threads.append(thread)
+
+    return threads
+
+
+def print_subreddits(threads):
+
+    for thread in threads:
+        print(f"r/{thread['subreddit']} - [{thread['upvotes']}] {thread['titulo']}")
+        print(f"\tLink: {thread['link']}")
+        print(f"\tComments: {thread['comments']}\n")
+
+if __name__ == '__main__':
+    print_subreddits(get_subreddit("Python"))
