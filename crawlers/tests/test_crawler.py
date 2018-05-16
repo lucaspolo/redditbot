5e41d51129383fd28b56aaa2fd5792cd931c66b4
Lucas Polo <pradopolo@Wgmail.com>
2018-05-15 23:23:00 -0300

    Aumentando cobertura de testes e refatorando para extrair métodos.

--- crawlers/tests/test_crawler.py
+++ crawlers/tests/test_crawler.py
@@ -1,5 +1,49 @@
-from crawlers.reddit_crawler import convert_upvotes_to_num
+from crawlers.reddit_crawler import convert_upvotes_to_num, filter_by_votes, convert_element_to_thread
 
 
-def test_convert_upvotes_to_num():
+def test_convert_upvotes_to_num():  # pragma: no cover
     assert convert_upvotes_to_num("10k") == 10000
+
+
+def test_filter_by_upvotes():  # pragma: no cover
+    threads = [
+        {
+            'upvotes': 1,
+        },
+        {
+            'upvotes': 100,
+        },
+        {
+            'upvotes': 200,
+        },
+        {
+            'upvotes': 300,
+        },
+    ]
+
+    assert len(filter_by_votes(threads, min_votes=150)) == 2
+
+
+def test_convert_element_to_thread():  # pragma: no cover
+    dados = {
+        'data': {
+            'subreddit': 'r/python',
+            'title': 'python',
+            'ups': '1',
+            'permalink': '/r/python/comments',
+            'url': 'https://www.google.com',
+        }
+    }
+
+    thread = convert_element_to_thread(dados)
+    assert thread['subreddit'] == 'r/python'
+    assert thread['title'] == 'python'
+    assert thread['upvotes'] == '1'
+    assert thread['comments'] == 'https://www.reddit.com/r/python/comments'
+    assert thread['link'] == 'https://www.google.com'
+
+    dados['data']['url'] = '/r/batata'
+
+    thread = convert_element_to_thread(dados)
+
+    assert thread['link'] == 'https://www.reddit.com/r/batata'
