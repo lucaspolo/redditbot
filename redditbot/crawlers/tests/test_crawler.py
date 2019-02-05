11a2ddb7c127e362d4c683cc3ed14f7a785cc249
Lucas Polo <pradopolo@gmail.com>
2019-02-04 22:00:24 -0200

    Improving tests for reddit_crawler

--- redditbot/crawlers/tests/test_crawler.py
+++ redditbot/crawlers/tests/test_crawler.py
@@ -1,5 +1,10 @@
+import re
+from unittest import mock
+
+import responses
+
 from redditbot.crawlers.reddit_crawler import filter_by_votes, \
-    convert_element_to_thread, convert_internal_link_to_absolute
+    convert_element_to_thread, convert_internal_link_to_absolute, get_threads, print_subreddits
 
 
 def test_filter_by_upvotes():
@@ -39,3 +44,46 @@ def test_convert_element_to_thread(dados):
 
 def test_convert_internal_link_to_absolute():
     assert convert_internal_link_to_absolute('/r/python/comments') == 'https://www.reddit.com/r/python/comments'
+
+
+@responses.activate
+def test_get_threads():
+    responses.add(
+        responses.GET,
+        re.compile('https://www.reddit.com/r/dogs/top.json'),
+        json={
+            'data': {
+                'children': [
+                    {
+                        'data': {
+                            'subreddit': 'dogs',
+                            'title': 'Cute Dogs',
+                            'ups': 999,
+                            'permalink': '/r/cute_dogs',
+                            'url': '/r/cutedogs'
+                        }
+                    }
+                ]
+            }
+        },
+        status=200
+    )
+
+    threads = get_threads("dogs")
+
+    assert len(threads) == 1
+
+
+@mock.patch('builtins.print')
+def test_print_subreddits(print_mock):
+    thread = {
+        'subreddit': 'Dog',
+        'upvotes': 999,
+        'title': 'Cute Dogs',
+        'link': 'http://doguitos.com',
+        'comments': 'http://comments.com'
+    }
+
+    print_subreddits([thread])
+
+    assert print_mock.call_count == 3
