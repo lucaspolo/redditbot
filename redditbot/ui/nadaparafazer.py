8d9289ed349b5dbdcb25ac27896e4aae7a0b9e99
Lucas Polo <pradopolo@gmail.com>
2019-08-30 23:17:33 -0300

    Convert the code to async requests, improving execution time

--- redditbot/ui/nadaparafazer.py
+++ redditbot/ui/nadaparafazer.py
@@ -1,16 +1,18 @@
+import asyncio
+
 import click
 
-from redditbot.crawlers.reddit_crawler import print_subreddits, filter_by_votes, get_threads
+from redditbot.crawlers.reddit_crawler import print_subreddits, filter_by_votes, get_subreddits
 
 
 @click.command()
 @click.option('--subreddits', '-s', required=True, help="Threads que deseja buscar separadas por ';'")
 @click.option('--min-votes', '-m', default=5000, help='Número mínimo de votos')
 def main(subreddits, min_votes):
-    for subreddit in subreddits.split(';'):
-        threads = get_threads(subreddit)
-        filtred_threads = filter_by_votes(threads, min_votes=min_votes)
-        print_subreddits(filtred_threads)
+    subreddits = subreddits.split(';')
+    threads = asyncio.run(get_subreddits(subreddits))
+    filtred_threads = filter_by_votes(threads, min_votes=min_votes)
+    print_subreddits(filtred_threads)
 
 
 if __name__ == '__main__':
