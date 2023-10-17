60433e916a4e179f3a08e421df09c0d2be8a8d49
Lucas Polo <lucaspolo@jusbrasil.com.br>
2023-10-17 10:29:07 -0300

    chore: change project to use HTTPX instead aiohttp

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,52 +1,47 @@
 import pytest
-from aioresponses import aioresponses
 
 
 @pytest.fixture
-def mock_request_dog():
-    with aioresponses() as m:
-        m.get(
-            url='https://www.reddit.com/r/dogs/top.json?sort=new',
-            status=200,
-            payload={
-                'data': {
-                    'children': [
-                        {
-                            'data': {
-                                'subreddit': 'dogs',
-                                'title': 'Cute Dogs',
-                                'ups': 9999,
-                                'permalink': '/r/cute_dogs',
-                                'url': '/r/cutedogs'
-                            }
+def mock_request_dog(httpx_mock):
+    httpx_mock.add_response(
+        method='GET',
+        url='https://www.reddit.com/r/dogs/top.json?sort=new',
+        json={
+            'data': {
+                'children': [
+                    {
+                        'data': {
+                            'subreddit': 'dogs',
+                            'title': 'Cute Dogs',
+                            'ups': 9999,
+                            'permalink': '/r/cute_dogs',
+                            'url': '/r/cutedogs'
                         }
-                    ]
-                }
-            },
-        )
-        yield m
+                    }
+                ]
+            }
+        },
+    )
 
 
 @pytest.fixture
-def mock_request_dog_with_low_votes():
-    with aioresponses() as m:
-        m.get(
-            url='https://www.reddit.com/r/dogs/top.json?sort=new',
-            status=200,
-            payload={
-                'data': {
-                    'children': [
-                        {
-                            'data': {
-                                'subreddit': 'dogs',
-                                'title': 'Cute Dogs',
-                                'ups': 1,
-                                'permalink': '/r/cute_dogs',
-                                'url': '/r/cutedogs'
-                            }
+def mock_request_dog_with_low_votes(httpx_mock):
+    httpx_mock.add_response(
+        method='GET',
+        url='https://www.reddit.com/r/dogs/top.json?sort=new',
+        json={
+            'data': {
+                'children': [
+                    {
+                        'data': {
+                            'subreddit': 'dogs',
+                            'title': 'Cute Dogs',
+                            'ups': 1,
+                            'permalink': '/r/cute_dogs',
+                            'url': '/r/cutedogs'
                         }
-                    ]
-                }
-            },
-        )
-        yield m
+                    }
+                ]
+            }
+        },
+    )
